240b11c0ab936e8dbae68ca25fafa30edc6a334a
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-09-25 03:08:05 +0900

    add: 実装追加

---
+++ api/helloworld1/index.ts
@@ -0,0 +1,14 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    await sleep(180000); // 3min
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "hello world 1"
+    };
+};
+
+export default httpTrigger;
